a855212d6dd4e1a9dfc707a8e282d2c52b550bcb
Vinodkumar-Gelot <vinodanajigelot@gmail.com>
2022-07-16 13:52:11 +0530

    m

--- IntermiaditeGit/Controllers/HomeController.cs
+++ IntermiaditeGit/Controllers/HomeController.cs
@@ -15,6 +15,7 @@ namespace IntermiaditeGit.Controllers
 
         public ActionResult About()
         {
+            //Commiting chnages in dev branch
             //Commiting chnages in dev branch t g
             ViewBag.Message = "Your application description page.";
             //a new commite
